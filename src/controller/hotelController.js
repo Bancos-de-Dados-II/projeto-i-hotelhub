5ea85903aa6aa09f5f0ad747234645a70cbb5ebe
Karlos Messyas <karlosmessyas14@gmail.com>
2024-08-12 23:57:29 -0300

    Finalizando CRUD de Hotel

--- src/controller/hotelController.js
+++ src/controller/hotelController.js
@@ -1,8 +1,66 @@
 import Hotel from "../models/Hotel.js";
 
 const adicionarHotel = async(req, res) => {
-    const hotel = await Hotel.create(req.body);
-    res.status(201).json(hotel);
+    try {
+        const hotel = await Hotel.create(req.body);
+        res.status(201).json(hotel);
+    } catch (error) {
+        res.status(400).json('Error');
+    }
 }
 
-export { adicionarHotel };
+const listarHoteis = async(req, res) => {
+    try {
+        const hoteis = await Hotel.findAll();
+        res.status(201).json(hoteis);
+    } catch (error) {
+        res.status(400).json('Error ao listar hotéis');
+    }
+}
+
+const listarHoteisPorCNPJ = async(req, res) => { 
+    try {
+        const cnpj = req.params.cnpj;
+        const hoteisPorCnpj = await Hotel.findOne({
+            where: {
+                cnpj,
+            }
+        });
+        res.status(201).json(hoteisPorCnpj);
+    } catch (error) {
+        res.status(400).json('Hotel não existe!');
+    }
+}
+
+const excluirHotel = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const hotelExluir = await Hotel.findByPk(id);
+        hotelExluir.destroy();
+        res.status(201).json('Hotel exluido');
+    } catch (error) {
+        res.status(400).json('Não foi possível excluir hotel');
+    }
+}
+
+const atualizarHotel = async(req, res) => {
+    const { nome, localizacao } = req.body;
+    try {
+        const id = req.params.id;
+
+        const hotelAtualizar = await Hotel.findByPk(id);
+
+        hotelAtualizar.nome = nome;
+        hotelAtualizar.localizacao = localizacao;
+
+        await hotelAtualizar.save();
+
+        res.status(201).json(hotelAtualizar);
+
+    } catch (error) {
+        res.status(400).json('Não foi possível atualizar o hotel');
+    }
+}
+
+
+export { adicionarHotel, listarHoteis, listarHoteisPorCNPJ, excluirHotel, atualizarHotel };
--- src/routes/hotelRoutes.js
+++ src/routes/hotelRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { adicionarHotel } from '../controller/hotelController.js'
+import { adicionarHotel, listarHoteis, listarHoteisPorCNPJ, excluirHotel, atualizarHotel } from '../controller/hotelController.js'
 
 const routes = express.Router();
 
 routes.post('/', adicionarHotel);
+routes.get('/', listarHoteis);
+routes.get('/:cnpj', listarHoteisPorCNPJ);
+routes.delete('/:id', excluirHotel);
+routes.put('/:id', atualizarHotel);
 
 export default routes;
 
