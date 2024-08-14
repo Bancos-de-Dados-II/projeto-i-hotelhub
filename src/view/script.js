6d193a7940aa9f9513aa448c74813ff1450c4565
Karlos Messyas <karlosmessyas14@gmail.com>
2024-08-13 22:42:46 -0300

    Adicionando método para buscar hotéis

--- src/view/script.js
+++ src/view/script.js
@@ -6,9 +6,9 @@ let map = L.map('map', {
 });
 
 let houseIcon = L.icon({
-    iconUrl: 'https://cdn-icons-png.flaticon.com/256/4064/4064836.png',
-    iconSize: [50, 50], // Tamanho do ícone
-    iconAnchor: [25, 50], // Ponto do ícone que corresponde à localização do marcador
+    iconUrl: 'https://cdn-icons-png.flaticon.com/512/2286/2286054.png',
+    iconSize: [50, 50],
+    iconAnchor: [25, 50], 
 });
 
 let marker = L.marker([-6.887698002563706, -38.56015173326553], {
@@ -70,3 +70,82 @@ button.addEventListener('click', () => {
         alert('Erro ao registrar hotel. Consulte o console para mais detalhes.');
     });
 });
+
+function listarHoteis() {
+    fetch('http://localhost:3000/hotel')
+        .then(response => response.json())
+        .then(data => {
+            console.log('Dados recebidos:', data); // Verifica se os dados estão corretos
+            displayHoteis(data);
+        })
+        .catch(e => {
+            console.error('Erro ao buscar hotéis:', e);
+        });
+}
+
+
+function displayHoteis (hotel) {
+    const dados = document.getElementById('dados');
+    dados.innerHTML = '';
+    console.log(hotel);
+    hotel.forEach(e => {
+        const tr = document.createElement('tr');
+
+        const id = document.createElement('td');
+
+        const nome = document.createElement('td');
+
+        const cnpj = document.createElement('td');
+
+        const latitude = document.createElement('td');
+
+        const longitude = document.createElement('td');
+
+        const tdButtonEdit = document.createElement('td');
+        const buttonEdit = document.createElement('button');
+        const linkButtonEdit = document.createElement('a');
+        const imgButtonEdit = document.createElement('img');
+
+        const tdButtonDelete = document.createElement('td');
+        const buttonDelete = document.createElement('button');
+        const imgButtonDelete = document.createElement('img');
+
+        linkButtonEdit.href = `./editHotel.html?${e.id}`
+
+        imgButtonEdit.classList.add('IconEditDele');
+        imgButtonEdit.id = 'icon-edit-hotel'
+        imgButtonEdit.src = './icons/pen.png'
+
+        imgButtonDelete.classList.add('IconEditDelete');
+        imgButtonDelete.id = 'icon-delete-hotel'
+        imgButtonDelete.src = './icons/delete.png'
+
+        id.textContent = e.id;
+        nome.textContent = e.nome;
+        cnpj.textContent = e.cnpj;
+        latitude.textContent = e.localizacao.coordinates[1];
+        longitude.textContent = e.localizacao.coordinates[0];
+
+        buttonEdit.appendChild(imgButtonEdit);
+        buttonDelete.appendChild(imgButtonDelete);
+
+        buttonDelete.dataset.id = e.id;
+
+        tdButtonDelete.appendChild(buttonDelete);
+        tdButtonEdit.appendChild(buttonEdit);
+
+        tr.appendChild(id);
+        tr.appendChild(nome);
+        tr.appendChild(cnpj);
+        tr.appendChild(latitude);
+        tr.appendChild(longitude);
+        tr.appendChild(tdButtonEdit);
+        tr.appendChild(tdButtonDelete);
+
+        dados.appendChild(tr);
+    });
+}
+
+listarHoteis();
+
+
